8025f6f558f8bc0786e513e7a6c8774500662846
Ivo Karaneshev <ivo.i.karaneshev@gmail.com>
2017-10-22 17:50:57 +0300

    Update generator.py

--- folder_of_knowladge_py/generator.py
+++ folder_of_knowladge_py/generator.py
@@ -26,5 +26,11 @@ def ppl_g(n):
                 }
         yield person
 
+t1 = time.clock()
 print("Memory list(Before): {}b".format(sys.getsizeof(ppl_l(1000000))))
+t2 = time.clock()
+print("time for iteration: ", t2-t1)
+t1 = time.clock()
 print("Memory generator(Before): {}b".format(sys.getsizeof(ppl_g(1000000))))
+t2 = time.clock()
+print("time for iteration: ", t2-t1)
