e5895f13a688330b94d8cc3794132450d4f6a73a
Ivo Karaneshev <ivo.i.karaneshev@gmail.com>
2018-05-01 13:09:51 +0300

    Update dict_comprehention.py

--- folder_of_knowladge_py/dict_comprehention.py
+++ folder_of_knowladge_py/dict_comprehention.py
@@ -1,2 +1,6 @@
 numb = ['one', 'two', 'theee', 'four']
 print({numb[i]:i+1 for i in range(4)})
+
+def wordPower(word):
+    num = {ch: numb for numb, ch in enumerate('abcdefghijklmnopqrstuvwxyz', start=1)}
+    return sum([num[ch] for ch in word])
