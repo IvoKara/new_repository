908074556b6814431ae27ca9a408661099ba0dc1
Ivo Karaneshev <ivo.i.karaneshev@gmail.com>
2018-05-24 17:38:15 +0300

    Update permutations_example.py

--- CodeFight_exercices/itertools/permutations_example.py
+++ CodeFight_exercices/itertools/permutations_example.py
@@ -1,4 +1,7 @@
 from itertools import permutations
 
-def rockPaperScissors(players):
+def rockPaperScissors(players):  #who with who will battle
     return sorted(permutations(players,2))
+
+def kthPermutation(numbers, k):  #all possible combinations of numbers
+    return list(permutations(numbers))[k-1]
