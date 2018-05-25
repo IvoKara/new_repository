9daf3695385d3c5fa1a7794e33ca760e75cf38c0
Ivo Karaneshev <ivo.i.karaneshev@gmail.com>
2018-05-25 23:08:04 +0300

    Update xor_power.py

--- CodeFight_exercices/xor_power.py
+++ CodeFight_exercices/xor_power.py
@@ -1,5 +1,16 @@
+#element that only appear once
 def singleNumber(nums):
     res = 0
     for num in nums:
         res ^= num
     return res
+
+
+#swaps first with second, third with forth...
+def correctLineup(athletes):
+    return [athletes[x^1] for x in range(len(athletes))]
+
+# 1^0=1
+# 1^1=0
+# 1^2=3
+# 1^3=2
